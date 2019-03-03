892a66bfb7cbe41ddda7ecd81dc07e5467c53764
MateuMorro <47154205+MateuMorro@users.noreply.github.com>
2019-03-03 21:32:39 +0100

    Update LoadSensorsUsuari.py

--- Supervisat/entities/carsimulator/LoadSensorsUsuari.py
+++ Supervisat/entities/carsimulator/LoadSensorsUsuari.py
@@ -4,71 +4,71 @@ class LoadSensorsUsuari(object):
     def __init__(self,n):
         self.__sensors=[]
         if n==1:
-            with open('..\\Sensors\\SensorsCircuit3.csv') as cs:
+            with open('../Sensors/SensorsCircuit3.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
         if n==2:
-            with open('..\\Sensors\\SensorsCircuit4.csv') as cs:
+            with open('../Sensors/SensorsCircuit4.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
         if n==3:
-            with open('..\\Sensors\\SensorsCircuit11.csv') as cs:
+            with open('../Sensors/SensorsCircuit11.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==4:
-            with open('..\\Sensors\\SensorsCircuit6.csv') as cs:
+            with open('../Sensors/SensorsCircuit6.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==5:
-            with open('..\\Sensors\\SensorsCircuit1.csv') as cs:
+            with open('../Sensors/SensorsCircuit1.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==6:
-            with open('..\\Sensors\\SensorsCircuit2.csv') as cs:
+            with open('../Sensors/SensorsCircuit2.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==7:
-            with open('..\\Sensors\\SensorsCircuit5.csv') as cs:
+            with open('../Sensors/SensorsCircuit5.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==8:
-            with open('..\\Sensors\\SensorsCircuit7.csv') as cs:
+            with open('../Sensors/SensorsCircuit7.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==9:
-            with open('..\\Sensors\\SensorsCircuit8Cheste.csv') as cs:
+            with open('../Sensors/SensorsCircuit8Cheste.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==10:
-            with open('..\\Sensors\\SensorsCircuit10.csv') as cs:
+            with open('../Sensors/SensorsCircuit10.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==11:
-            with open('..\\Sensors\\SensorsCircuit9Catalunya.csv') as cs:
+            with open('../Sensors/SensorsCircuit9Catalunya.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
 
         if n==12:
-            with open('..\\Sensors\\SensorsCircuit12.csv') as cs:
+            with open('../Sensors/SensorsCircuit12.csv') as cs:
                 entrada = csv.reader(cs)
                 for i in entrada:
                     self.__sensors.append([[float(i[k])] for k in range(0, 11)])
